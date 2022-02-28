faa1cfa2a6831e4d654fbff081a02c5c83105126
Suresh <sureshkumar_a@outlook.com>
2022-02-28 13:04:39 +0530

    Added amazonTest2

--- src/test/java/com/uiautomation/tests/AmazonTests.java
+++ src/test/java/com/uiautomation/tests/AmazonTests.java
@@ -25,4 +25,14 @@ public final class AmazonTests extends BaseTest{
 		
 	}
 	
+	@FrameworkAnnotation(author = {"Suresh","Kumar"},category= {CategoryType.SMOKE,CategoryType.REGRESSION})
+	@Test
+	public void amazonTest2(Map<String,String> data) throws Exception {
+		
+		String title = new AmazonHomePage().clickHamburger().ClickComputers().ClickOnSubMenuItem(data.get("menutext")).getTitle();
+		
+		Assertions.assertThat(title).isNotNull().isNotBlank();	
+		
+	}
+	
 }
